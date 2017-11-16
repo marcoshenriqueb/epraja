7048a41e9493e00922fd8c2f21a61b9220680bda
Marcos Almeida <marcoshenriqueb@gmail.com>
2017-11-16 17:48:45 -0200

    Adds react router

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,13 @@ module.exports = {
   'rules': {
     // Recognizes JSX components vars used.
     "react/jsx-uses-vars": "error",
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "rules": {
+      "jsx-a11y/anchor-is-valid": [ "error", {
+        "components": [ "Link" ],
+        "specialLink": [ "hrefLeft", "hrefRight" ],
+        "aspects": [ "noHref", "invalidHref", "preferButton" ]
+      }]
+    }
   }
 };
